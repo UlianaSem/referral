5310450a9d7c19aeeec0be1f02b75b5b01ecc5b7
Uliana Semyonova <i@ulianasemyonova.ru>
2024-04-24 12:14:40 +0500

    Create Users model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+
+    phone = models.CharField(max_length=12, verbose_name='Телефон', unique=True)
+    verification_code = models.CharField(max_length=4, verbose_name='Проверочный код',
+                                         **NULLABLE)
+    own_code = models.CharField(max_length=6, verbose_name='Личный инвайт-код', unique=True)
+    entered_code = models.CharField(max_length=6, verbose_name='Введенный инвайт-код',
+                                    **NULLABLE)
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
